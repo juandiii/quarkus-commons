484f0dd5f606b0d636b2c8cbf2d67d7a8ff44b4f
Juan Diego <juandiegolopezve@gmail.com>
2020-05-26 17:29:14 -0400

    Adding dateAdapter for JSONB

--- src/main/java/xyz/juandiii/commons/auth/JwtAuth.java
+++ src/main/java/xyz/juandiii/commons/auth/JwtAuth.java
@@ -2,7 +2,7 @@ package xyz.juandiii.commons.auth;
 
 public interface JwtAuth extends UserInterface {
 
-    public UserInterface login(Credentials credentials);
-    public UserInterface register(UserInterface userInterface);
+    UserInterface login(Credentials credentials);
+    UserInterface register(UserInterface userInterface);
 
 }
